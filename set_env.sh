80bbaf8f5491e595fe3702582a499709c19f9ded
kerms <kerms@niazo.org>
2024-07-07 10:39:44 +0800

    fix(set_env) error:bad variable name, make compatible to sh

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-export VITE_APP_GIT_TAG=$(git describe --tags | cut -d'-' -f1,2)
-export VITE_APP_LAST_COMMIT=$(git log -1 --format=%cd)
+
+VITE_APP_GIT_TAG=$(git describe --tags | cut -d'-' -f1,2)
+VITE_APP_LAST_COMMIT=$(git log -1 --format=%cd)
+
+export VITE_APP_GIT_TAG
+export VITE_APP_LAST_COMMIT
